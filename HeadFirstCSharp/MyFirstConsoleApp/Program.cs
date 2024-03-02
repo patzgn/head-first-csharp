30a6243dd15c7c026de04f2626afe7d7de82158a
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-02 11:10:09 +0100

    My First Console App update

--- HeadFirstCSharp/MyFirstConsoleApp/Program.cs
+++ HeadFirstCSharp/MyFirstConsoleApp/Program.cs
@@ -1,16 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace MyFirstConsoleApp;
 
-namespace MyFirstConsoleApp
+internal class Program
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
-    }
+	static void Main(string[] args)
+	{
+		OperatorExamples();
+	}
+
+	private static void OperatorExamples()
+	{
+		// This statement declares a variable and sets it to 3
+		int width = 3;
+
+		// The ++ operator increments a variable (adds 1 to it)
+		width++;
+
+		// Declare two more int variables to hold numbers and
+		// use the + and * operators to add and multiply values
+		int height = 2 + 4;
+		int area = width * height;
+		Console.WriteLine(area);
+
+		while (area < 50)
+		{
+			height++;
+			area = width * height;
+		}
+
+		do
+		{
+			width--;
+			area = width * height;
+		} while (area > 25);
+
+		// The next two statements declare string variables
+		// and use + to concatenate them (join them together)
+		string result = "Powierzchnia";
+		result = result + " wynosi " + area;
+		Console.WriteLine(result);
+
+		// A Boolean variable is either true or false
+		bool truthValue = true;
+		Console.WriteLine(truthValue);
+	}
 }
