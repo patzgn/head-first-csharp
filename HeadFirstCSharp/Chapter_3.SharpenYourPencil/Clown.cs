51337ba4217cc9b1c551caf7b8f2a9d1df05e0bf
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-02 21:05:39 +0100

    Sharpen your pencil chapter 3

---
+++ HeadFirstCSharp/Chapter_3.SharpenYourPencil/Clown.cs
@@ -0,0 +1,13 @@
+﻿namespace Chapter_3.SharpenYourPencil;
+
+internal class Clown
+{
+	public string Name;
+	public int Height;
+
+	public void TalkAboutYourself()
+	{
+		Console.WriteLine("My name is " + Name +
+			" and I'm " + Height + " inches tall.");
+	}
+}
---
+++ HeadFirstCSharp/Chapter_3.SharpenYourPencil/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Chapter_3.SharpenYourPencil;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Clown oneClown = new();
+		oneClown.Name = "Boffo";
+		oneClown.Height = 172;
+		oneClown.TalkAboutYourself();
+
+		Clown anotherClown = new();
+		anotherClown.Name = "Biff";
+		anotherClown.Height = 180;
+		anotherClown.TalkAboutYourself();
+
+		Clown clown3 = new();
+		clown3.Name = anotherClown.Name;
+		clown3.Height = oneClown.Height - 3;
+		clown3.TalkAboutYourself();
+
+		anotherClown.Height *= 2;
+		anotherClown.TalkAboutYourself();
+	}
+}
