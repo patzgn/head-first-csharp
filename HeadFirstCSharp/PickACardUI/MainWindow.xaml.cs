acffff7c8cbeb1492d201e71b4f415992a86c56b
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-02 20:41:46 +0100

    Pick a card WPF

---
+++ HeadFirstCSharp/PickACardUI/MainWindow.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System.Windows;
+
+namespace PickACardUI
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			string[] pickedCards = CardPicker.PickSomeCards((int)numberOfCards.Value);
+
+			listOfCards.Items.Clear();
+			foreach (var card in pickedCards)
+			{
+				listOfCards.Items.Add(card);
+			}
+		}
+	}
+}
