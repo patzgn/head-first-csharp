ec3e2184366a2de6f1327b5ebb861addc334880c
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-03 08:03:24 +0100

    Casino

---
+++ HeadFirstCSharp/Casino/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace Casino;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Random random = new();
+		double odds = 0.75;
+
+		Guy player = new() { Name = "The player", Cash = 100 };
+
+		Console.WriteLine("Welcome to the casino. The odds are " + odds);
+		while (player.Cash > 0)
+		{
+			player.WriteMyInfo();
+
+			Console.Write("How much do you want to bet: ");
+			string howMuch = Console.ReadLine();
+			if (int.TryParse(howMuch, out int amount))
+			{
+				int pot = player.GiveCash(amount) * 2;
+				if (pot > 0)
+				{
+					if (random.NextDouble() > odds)
+					{
+						int winnings = pot;
+						Console.WriteLine("You win " + winnings);
+						player.ReceiveCash(winnings);
+					}
+					else
+					{
+						Console.WriteLine("Bad luck, you lose.");
+					}
+				}
+			}
+			else
+			{
+				Console.WriteLine("Please enter a valid number.");
+			}
+		}
+		Console.WriteLine("The house always wins.");
+	}
+}
