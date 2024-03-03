33e387de93b8637044565b2f78f8692bddbc1d0d
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-03 09:12:11 +0100

    Pick a Card Blazor

---
+++ HeadFirstCSharp/Chapter_3.PickACardBlazor/CardPicker.cs
@@ -0,0 +1,38 @@
+﻿namespace Chapter_3.PickACardBlazor;
+
+internal class CardPicker
+{
+	private static Random random = new();
+
+	public static string[] PickSomeCards(int numberOfCards)
+	{
+		string[] pickedCards = new string[numberOfCards];
+		for (int i = 0; i < numberOfCards; i++)
+		{
+			pickedCards[i] = RandomValue() + " " + RandomSuit();
+		}
+
+		return pickedCards;
+	}
+
+	private static string RandomSuit()
+	{
+		int value = random.Next(1, 5);
+
+		if (value == 1) return "Spades";
+		if (value == 2) return "Hearts";
+		if (value == 3) return "Clubs";
+		return "Diamonds";
+	}
+
+	private static string RandomValue()
+	{
+		int value = random.Next(1, 14);
+
+		if (value == 1) return "Ace";
+		if (value == 11) return "Jack";
+		if (value == 12) return "Queen";
+		if (value == 13) return "King";
+		return value.ToString();
+	}
+}
