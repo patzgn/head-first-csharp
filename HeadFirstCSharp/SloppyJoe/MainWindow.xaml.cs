9509dd2b495ae9b5986bfc6f16c7b0097ea29c2a
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-07 20:39:29 +0100

    Sloppy Joe

---
+++ HeadFirstCSharp/SloppyJoe/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System.Windows;
+
+namespace Chapter_4.SloppyJoe
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			MakeTheMenu();
+		}
+
+		private void MakeTheMenu()
+		{
+			MenuItem[] menuItems = new MenuItem[5];
+			string guacamolePrice;
+
+			for (int i = 0; i < 5; i++)
+			{
+				menuItems[i] = new MenuItem();
+				if (i >= 3)
+				{
+					menuItems[i].Breads = new string[]
+					{
+						 "plain bagel", "onion bagel", "pumpernickel bagel", "everything bagel"
+					};
+				}
+				menuItems[i].Generate();
+			}
+
+			item1.Text = menuItems[0].Description;
+			price1.Text = menuItems[0].Price;
+			item2.Text = menuItems[1].Description;
+			price2.Text = menuItems[1].Price;
+			item3.Text = menuItems[2].Description;
+			price3.Text = menuItems[2].Price;
+			item4.Text = menuItems[3].Description;
+			price4.Text = menuItems[3].Price;
+			item5.Text = menuItems[4].Description;
+			price5.Text = menuItems[4].Price;
+
+			MenuItem specialMenuItem = new()
+			{
+				Proteins = new string[] { "Organic ham", "Mushroom patty", "Mortadella" },
+				Breads = new string[] { "a gluten free roll", "a wrap", "pita" },
+				Condiments = new string[] { "dijon mustard", "miso dressing", "au jus" }
+			};
+			specialMenuItem.Generate();
+
+			item6.Text = specialMenuItem.Description;
+			price6.Text = specialMenuItem.Price;
+
+			MenuItem guacamoleMenuItem = new();
+			guacamoleMenuItem.Generate();
+			guacamolePrice = guacamoleMenuItem.Price;
+
+			guacamole.Text = "Add guacamole for " + guacamolePrice;
+		}
+	}
+}
