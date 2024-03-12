8300e5ae1469dc437f3139b252fb610b86f06107
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-12 18:54:34 +0100

    HiLo game

---
+++ Chapter-5/HiLo/HiLo/HiLoGame.cs
@@ -0,0 +1,39 @@
+﻿namespace HiLo;
+
+internal static class HiLoGame
+{
+	public const int MAXIMUM = 10;
+
+	private static Random random = new();
+	private static int currentNumber = random.Next(1, MAXIMUM + 1);
+	private static int nextNumber = random.Next(1, MAXIMUM + 1);
+	private static int pot = 10;
+
+	public static int GetPot() => pot;
+
+	public static void Guess(bool higher)
+	{
+		if ((higher && nextNumber >= currentNumber) || (!higher && nextNumber <= currentNumber))
+		{
+			Console.WriteLine("You guessed right!");
+			pot++;
+		}
+		else
+		{
+			Console.WriteLine("Bad luck, you guessed wrong.");
+			pot--;
+		}
+		currentNumber = nextNumber;
+		nextNumber = random.Next(1, MAXIMUM + 1);
+		Console.WriteLine($"The current number is {currentNumber}");
+	}
+
+	public static void Hint()
+	{
+		int half = MAXIMUM / 2;
+		if (nextNumber >= half)
+			Console.WriteLine($"The current number is {currentNumber}, the next number is at least {half}");
+		else Console.WriteLine($"The current number is {currentNumber}, the next is at most {half}");
+		pot--;
+	}
+}
