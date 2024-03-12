32d73483ff53598a756d6c4963cf168e49241132
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-12 19:40:38 +0100

    Paintball gun

---
+++ Chapter-5/PaintballGun/PaintballGun/PaintballGun.cs
@@ -0,0 +1,49 @@
+﻿namespace PaintballGun;
+
+internal class PaintballGun
+{
+	public int MagazineSize { get; private set; } = 16;
+
+	private int balls = 0;
+	public int Balls
+	{
+		get { return balls; }
+		set
+		{
+			if (value > 0)
+				balls = value;
+			Reload();
+		}
+	}
+
+	public int BallsLoaded { get; private set; }
+
+	public bool IsEmpty() => BallsLoaded == 0;
+
+	public void Reload()
+	{
+		if (balls > MagazineSize)
+		{
+			BallsLoaded = MagazineSize;
+		}
+		else
+		{
+			BallsLoaded = balls;
+		}
+	}
+
+	public bool Shoot()
+	{
+		if (BallsLoaded == 0) return false;
+		BallsLoaded--;
+		balls--;
+		return true;
+	}
+
+	public PaintballGun(int balls, int magazineSize, bool loaded)
+	{
+		this.balls = balls;
+		MagazineSize = magazineSize;
+		if (!loaded) Reload();
+	}
+}
