e270847b73086120a921256e5fa4d574a148986b
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-17 16:53:28 +0100

    Beehive Management System Blazor

---
+++ Chapter-6/BeehiveManagementSystemBlazor/BeehiveManagementSystemBlazor/Bee.cs
@@ -0,0 +1,22 @@
+﻿namespace BeehiveManagementSystemBlazor;
+
+abstract class Bee
+{
+	public abstract float CostPerShift { get; }
+	public string Job { get; private set; }
+
+	public Bee(string job)
+	{
+		Job = job;
+	}
+
+	public void WorkTheNextShift()
+	{
+		if (HoneyVault.ConsumeHoney(CostPerShift))
+		{
+			DoJob();
+		}
+	}
+
+	protected abstract void DoJob();
+}
---
+++ Chapter-6/BeehiveManagementSystemBlazor/BeehiveManagementSystemBlazor/HoneyVault.cs
@@ -0,0 +1,62 @@
+﻿namespace BeehiveManagementSystemBlazor;
+
+static class HoneyVault
+{
+	public const float NECTAR_CONVERSION_RATIO = .19f;
+	public const float LOW_LEVEL_WARNING = 10f;
+
+	private static float honey = 25f;
+	private static float nectar = 100f;
+
+	public static string StatusReport
+	{
+		get
+		{
+			string status = $"{honey:0.0} units of honey\n" +
+				$"{nectar:0.0} units of nectar";
+			string warnings = "";
+			if (honey < LOW_LEVEL_WARNING)
+			{
+				warnings += "\nLOW HONEY - ADD A HONEY MANUFACTURER";
+			}
+			if (nectar < LOW_LEVEL_WARNING)
+			{
+				warnings += "\nLOW NECTAR - ADD A NECTAR COLLECTOR";
+			}
+			return status + warnings;
+		}
+	}
+
+	public static void CollectNectar(float amount)
+	{
+		if (amount > 0)
+		{
+			nectar += amount;
+		}
+	}
+
+	public static void ConvertNectarToHoney(float amount)
+	{
+		if (amount > nectar)
+		{
+			nectar = 0;
+			honey += nectar * NECTAR_CONVERSION_RATIO;
+		}
+		else
+		{
+			nectar -= amount;
+			honey += amount * NECTAR_CONVERSION_RATIO;
+		}
+	}
+
+	public static bool ConsumeHoney(float amount)
+	{
+		if (amount > honey)
+		{
+			return false;
+		}
+
+		honey -= amount;
+		return true;
+	}
+}
