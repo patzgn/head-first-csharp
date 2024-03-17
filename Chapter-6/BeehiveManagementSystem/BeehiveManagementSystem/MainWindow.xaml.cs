096dd4b3b2f6a42b74908c55551742646062d023
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-17 16:06:06 +0100

    Beehive Management System WPF

---
+++ Chapter-6/BeehiveManagementSystem/BeehiveManagementSystem/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace BeehiveManagementSystem;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	private Queen queen = new();
+	private DispatcherTimer timer = new();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		statusReport.Text = queen.StatusReport;
+		timer.Tick += Timer_Tick;
+		timer.Interval = TimeSpan.FromSeconds(1.5);
+		timer.Start();
+	}
+
+	private void Timer_Tick(object sender, EventArgs e)
+	{
+		WorkShift_Click(this, new RoutedEventArgs());
+	}
+
+	private void AssignJob_Click(object sender, RoutedEventArgs e)
+	{
+		queen.AssignBee(jobSelector.Text);
+		statusReport.Text = queen.StatusReport;
+	}
+
+	private void WorkShift_Click(object sender, RoutedEventArgs e)
+	{
+		queen.WorkTheNextShift();
+		statusReport.Text = queen.StatusReport;
+	}
+}
---
+++ Chapter-6/BeehiveManagementSystem/BeehiveManagementSystem/Queen.cs
@@ -0,0 +1,94 @@
+﻿namespace BeehiveManagementSystem;
+
+class Queen : Bee
+{
+	public const float EGGS_PER_SHIFT = 0.45f;
+	public const float HONEY_PER_UNASSIGNED_WORKER = 0.5f;
+
+	public string StatusReport { get; private set; }
+	public override float CostPerShift { get { return 2.15f; } }
+
+	private Bee[] workers = new Bee[0];
+	private float eggs;
+	private float unassignedWorkers = 3;
+
+	public Queen() : base("Queen")
+	{
+		AssignBee("Nectar Collector");
+		AssignBee("Honey Manufacturer");
+		AssignBee("Egg Care");
+	}
+
+	public void AssignBee(string job)
+	{
+		switch (job)
+		{
+			case "Nectar Collector":
+				AddWorker(new NectarCollector());
+				break;
+			case "Honey Manufacturer":
+				AddWorker(new HoneyManufacturer());
+				break;
+			case "Egg Care":
+				AddWorker(new EggCare(this));
+				break;
+		}
+		UpdateStatusReport();
+	}
+
+	public void CareForEggs(float eggsToConvert)
+	{
+		if (eggs >= eggsToConvert)
+		{
+			eggs -= eggsToConvert;
+			unassignedWorkers += eggsToConvert;
+		}
+	}
+
+	protected override void DoJob()
+	{
+		eggs += EGGS_PER_SHIFT;
+		foreach (Bee worker in workers)
+		{
+			worker.WorkTheNextShift();
+		}
+		HoneyVault.ConsumeHoney(HONEY_PER_UNASSIGNED_WORKER * unassignedWorkers);
+		UpdateStatusReport();
+	}
+
+	private void AddWorker(Bee worker)
+	{
+		if (unassignedWorkers >= 1)
+		{
+			unassignedWorkers--;
+			Array.Resize(ref workers, workers.Length + 1);
+			workers[workers.Length - 1] = worker;
+		}
+	}
+
+	private void UpdateStatusReport()
+	{
+		StatusReport = $"Vault report:\n{HoneyVault.StatusReport}\n" +
+			$"\nEgg count: {eggs:0.0}\nUnassigned workers: {unassignedWorkers:0.0}\n" +
+			$"{WorkerStatus("Nectar Collector")}\n{WorkerStatus("Honey Manufacturer")}" +
+			$"\n{WorkerStatus("Egg Care")}\nTOTAL WORKERS: {workers.Length}";
+	}
+
+	private string WorkerStatus(string job)
+	{
+		int count = 0;
+		foreach (Bee worker in workers)
+		{
+			if (worker.Job == job)
+			{
+				count++;
+			}
+		}
+		string s = "s";
+		if (count == 1)
+		{
+			s = "";
+		}
+		return $"{count} {job} bee{s}";
+	}
+}
