d7b70510c9b1e77353c971d7bcb331993a62ab4c
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-20 20:15:28 +0100

    Beehive Management System

---
+++ Chapter-7/BeehiveManagementSystem/BeehiveManagementSystem/Bee.cs
@@ -0,0 +1,22 @@
+﻿namespace BeehiveManagementSystem;
+
+abstract class Bee : IWorker
+{
+	public abstract float CostPerShift { get; }
+	public string Job { get; private set; }
+
+	public Bee(string job)
+	{
+		Job = job;
+	}
+
+	public void WorkTheNextShift()
+	{
+		if (HoneyVault.ConsumeHoney(CostPerShift))
+		{
+			DoJob();
+		}
+	}
+
+	protected abstract void DoJob();
+}
---
+++ Chapter-7/BeehiveManagementSystem/BeehiveManagementSystem/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace BeehiveManagementSystem;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+	private Queen queen;
+	private DispatcherTimer timer = new();
+
+	public MainWindow()
+	{
+		InitializeComponent();
+		queen = Resources["queen"] as Queen;
+		timer.Tick += Timer_Tick;
+		timer.Interval = TimeSpan.FromSeconds(1.5);
+		timer.Start();
+	}
+
+	private void Timer_Tick(object sender, EventArgs e)
+	{
+		WorkShift_Click(this, new RoutedEventArgs());
+	}
+
+	private void AssignJob_Click(object sender, RoutedEventArgs e)
+	{
+		queen.AssignBee(jobSelector.Text);
+	}
+
+	private void WorkShift_Click(object sender, RoutedEventArgs e)
+	{
+		queen.WorkTheNextShift();
+	}
+}
