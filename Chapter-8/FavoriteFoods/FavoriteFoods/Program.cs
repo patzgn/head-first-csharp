3d29462e409ffb35e537b4569c84d4f158fa28a5
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-29 17:39:06 +0100

    Favorite foods

---
+++ Chapter-8/FavoriteFoods/FavoriteFoods/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace FavoriteFoods;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Dictionary<string, string> favoriteFoods = new();
+		favoriteFoods["Alex"] = "hot dogs";
+		favoriteFoods["A'ja"] = "pizza";
+		favoriteFoods["Jules"] = "falafel";
+		favoriteFoods["Naima"] = "spaghetti";
+
+		string name;
+		while ((name = Console.ReadLine()) != "")
+		{
+			if (favoriteFoods.ContainsKey(name))
+			{
+				Console.WriteLine($"{name}'s favorite food is {favoriteFoods[name]}");
+			}
+			else
+			{
+				Console.WriteLine($"I don't know {name}'s favorite food");
+			}
+		}
+	}
+}
