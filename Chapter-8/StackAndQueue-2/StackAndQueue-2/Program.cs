c921da7d6975fbe83cc5fa831e656364c7c08534
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-29 18:06:37 +0100

    Stack and queue 2

---
+++ Chapter-8/StackAndQueue-2/StackAndQueue-2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace StackAndQueue_2;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Stack<string> myStack = new();
+		myStack.Push("first in line");
+		myStack.Push("second in line");
+		myStack.Push("third in line");
+		myStack.Push("last in line");
+
+		Queue<string> myQueue = new(myStack);
+		List<string> myList = new(myQueue);
+		Stack<string> anotherStack = new(myList);
+
+		Console.WriteLine($@"myQueue has {myQueue.Count} items
+myList has {myList.Count} items
+anotherStack has {anotherStack.Count} items");
+	}
+}
