ee769f92e0a6af4df5da763b5ad671fb197258c1
patzgn <103074854+patzgn@users.noreply.github.com>
2024-03-29 17:45:52 +0100

    Baseball players

---
+++ Chapter-8/BaseballPlayers/BaseballPlayers/RetiredPlayer.cs
@@ -0,0 +1,13 @@
+﻿namespace BaseballPlayers;
+
+internal class RetiredPlayer
+{
+	public string Name { get; private set; }
+	public int YearRetired { get; private set; }
+
+	public RetiredPlayer(string player, int yearRetired)
+	{
+		Name = player;
+		YearRetired = yearRetired;
+	}
+}
