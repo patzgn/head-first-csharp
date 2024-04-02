8facb1a191f63802717872a658ef3b7722f2adf3
patzgn <103074854+patzgn@users.noreply.github.com>
2024-04-02 18:48:32 +0200

    Linq test

---
+++ Chapter-9/LinqTest/LinqTest/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LinqTest;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = new();
+        for (int i = 1; i <= 99; i++)
+        {
+            numbers.Add(i);
+        }
+        IEnumerable<int> firstAndLastFive =
+            numbers.Take(5).Concat(numbers.TakeLast(5));
+        foreach (int i in firstAndLastFive)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
