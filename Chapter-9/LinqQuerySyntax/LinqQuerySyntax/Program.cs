ca7c30b55dead5984b1ea340cdc748866c76c03a
patzgn <103074854+patzgn@users.noreply.github.com>
2024-04-02 19:21:24 +0200

    Linq query syntax

---
+++ Chapter-9/LinqQuerySyntax/LinqQuerySyntax/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace LinqQuerySyntax;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        int[] values = new int[] { 0, 12, 44, 36, 92, 54, 13, 8 };
+
+        IEnumerable<int> result =
+            from v in values
+            where v < 37
+            orderby -v
+            select v;
+
+        foreach (int i in result)
+        {
+            Console.Write($"{i} ");
+        }
+    }
+}
