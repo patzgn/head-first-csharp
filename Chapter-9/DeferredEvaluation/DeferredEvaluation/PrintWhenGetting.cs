c2f14552f739ed4fa8d5cb9e16ba11615390008f
patzgn <103074854+patzgn@users.noreply.github.com>
2024-04-07 14:37:16 +0200

    Deferred evaluation

---
+++ Chapter-9/DeferredEvaluation/DeferredEvaluation/PrintWhenGetting.cs
@@ -0,0 +1,15 @@
+﻿namespace DeferredEvaluation;
+
+internal class PrintWhenGetting
+{
+	private int instanceNumber;
+	public int InstanceNumber
+	{
+		set { instanceNumber = value; }
+		get
+		{
+			Console.WriteLine($"Getting #{instanceNumber}");
+			return instanceNumber;
+		}
+	}
+}
---
+++ Chapter-9/DeferredEvaluation/DeferredEvaluation/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DeferredEvaluation;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var listOfObjects = new List<PrintWhenGetting>();
+		for (int i = 0; i < 5; i++)
+		{
+			listOfObjects.Add(new PrintWhenGetting() { InstanceNumber = i });
+		}
+
+		Console.WriteLine("Set up the query");
+		var result =
+			from o in listOfObjects
+			select o.InstanceNumber;
+
+		Console.WriteLine("Run the foreach");
+		foreach (var number in result)
+		{
+			Console.WriteLine($"Writing #{number}");
+		}
+
+		var immediate = result.ToList();
+
+		Console.WriteLine("Run the foreach");
+		foreach (var number in immediate)
+		{
+			Console.WriteLine($"Writing #{number}");
+		}
+	}
+}
