ad78c5876a9ec9609965ca4e5932fcc04e142a74
patzgn <103074854+patzgn@users.noreply.github.com>
2024-05-07 20:04:41 +0200

    Jimmy LINQ refactored

---
+++ Chapter-9/JimmyLinqRefactored/JimmyLinq/ComicAnalyzer.cs
@@ -0,0 +1,34 @@
+﻿namespace JimmyLinq;
+
+public static class ComicAnalyzer
+{
+	public static IEnumerable<string> GetReviews(
+		IEnumerable<Comic> catalog, IEnumerable<Review> reviews)
+	{
+		return catalog
+			.OrderBy(comic => comic.Issue)
+			.Join(
+				reviews,
+				comic => comic.Issue,
+				review => review.Issue,
+				(comic, review) =>
+					$"{review.Critic} rated #{comic.Issue} '{comic.Name}' {review.Score:0.00}");
+	}
+
+	public static IEnumerable<IGrouping<PriceRange, Comic>> GroupComicsByPrice(
+		IEnumerable<Comic> catalog, IReadOnlyDictionary<int, decimal> prices)
+	{
+		return catalog
+			.OrderBy(comic => prices[comic.Issue])
+			.GroupBy(comic => CalculatePriceRange(comic, prices));
+	}
+
+	private static PriceRange CalculatePriceRange(Comic comic, IReadOnlyDictionary<int, decimal> prices)
+	{
+		if (prices[comic.Issue] < 100)
+		{
+			return PriceRange.Cheap;
+		}
+		return PriceRange.Expensive;
+	}
+}
