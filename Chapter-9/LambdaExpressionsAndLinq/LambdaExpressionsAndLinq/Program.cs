5b27caaf723f0479ca5726a4a377811ef71ba086
patzgn <103074854+patzgn@users.noreply.github.com>
2024-05-07 19:03:56 +0200

    Lambda expressions and LINQ

---
+++ Chapter-9/LambdaExpressionsAndLinq/LambdaExpressionsAndLinq/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace LambdaExpressionsAndLinq;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var array = new[] { 1, 2, 3, 4 };
+		var result = array.Select(i => i * 2);
+		foreach (var i in result) Console.WriteLine(i);
+	}
+}
