8f29b61ea6d5023198fb7293dfe313817b23c127
patzgn <103074854+patzgn@users.noreply.github.com>
2024-05-07 18:35:57 +0200

    Lambda test drive

---
+++ Chapter-9/LambdaTestDrive/LambdaTestDrive/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace LambdaTestDrive;
+
+internal class Program
+{
+	static Random Random => new();
+
+	static double GetRandomDouble(int max) => max * Random.NextDouble();
+
+	static void PrintValue(double d) => Console.WriteLine($"The value is {d:0.0000}.");
+
+	static void Main(string[] args)
+	{
+		var value = GetRandomDouble(100);
+		PrintValue(value);
+	}
+}
