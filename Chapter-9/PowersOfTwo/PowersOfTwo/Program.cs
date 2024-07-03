8a8d61fc9b8b1e47a7b1dce3bc9c2051b0bbd21e
patzgn <103074854+patzgn@users.noreply.github.com>
2024-07-03 19:46:57 +0200

    Powers of two

---
+++ Chapter-9/PowersOfTwo/PowersOfTwo/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace PowersOfTwo;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var powers = new PowersOfTwo();
+		foreach (var p in powers)
+		{
+			Console.WriteLine(p);
+		}
+	}
+}
