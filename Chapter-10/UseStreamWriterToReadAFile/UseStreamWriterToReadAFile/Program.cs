65d093cc70d665955e4062fba40ec4972bd70ba7
patzgn <103074854+patzgn@users.noreply.github.com>
2024-08-12 20:13:48 +0200

    Use Stream Writer To Read A File

---
+++ Chapter-10/UseStreamWriterToReadAFile/UseStreamWriterToReadAFile/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace UseStreamWriterToReadAFile;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var folder = Environment.SpecialFolder.Personal;
+
+		var reader = new StreamReader($"{folder}{Path.DirectorySeparatorChar}secret_plan.txt");
+		var writer = new StreamWriter($"{folder}{Path.DirectorySeparatorChar}emailToCaptainA.txt");
+
+		writer.WriteLine("To: CaptainAmazing@objectville.net");
+		writer.WriteLine("From: Commissioner@objectville.net");
+		writer.WriteLine("Subject: Can you save the day... again?");
+		writer.WriteLine();
+		writer.WriteLine("We've discovered the Swindler's terrible plan:");
+
+		while (!reader.EndOfStream)
+		{
+			var lineFromThePlan = reader.ReadLine();
+			writer.WriteLine($"The plan -> {lineFromThePlan}");
+		}
+
+		writer.WriteLine();
+		writer.WriteLine("Can you help us?");
+
+		writer.Close();
+		reader.Close();
+	}
+}
