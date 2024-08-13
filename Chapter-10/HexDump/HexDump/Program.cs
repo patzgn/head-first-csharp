ab1efc463c4c3394b9920c4995d3eef479ce1201
patzgn <103074854+patzgn@users.noreply.github.com>
2024-08-13 20:06:10 +0200

    Hex Dump

---
+++ Chapter-10/HexDump/HexDump/Program.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace HexDump;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		var position = 0;
+		using (Stream input = File.OpenRead(args[0]))
+		{
+			var buffer = new byte[16];
+			int bytesRead;
+
+			while ((bytesRead = input.Read(buffer, 0, buffer.Length)) > 0)
+			{
+				Console.Write("{0:x4}: ", position);
+				position += bytesRead;
+
+				for (int i = 0; i < 16; i++)
+				{
+					if (i < bytesRead)
+					{
+						Console.Write("{0:x2} ", buffer[i]);
+					}
+					else
+					{
+						Console.Write("   ");
+					}
+					if (i == 7)
+					{
+						Console.Write("-- ");
+					}
+
+					if (buffer[i] < 0x20 || buffer[i] > 0x7F) buffer[i] = (byte)'.';
+				}
+
+				var bufferContents = Encoding.UTF8.GetString(buffer);
+				Console.WriteLine("   {0}", bufferContents.Substring(0, bytesRead));
+			}
+		}
+	}
+}
