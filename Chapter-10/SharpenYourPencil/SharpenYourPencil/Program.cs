0c8cd46206686336b6a37dafbec12cb39da8f3b7
patzgn <103074854+patzgn@users.noreply.github.com>
2024-08-13 18:38:48 +0200

    Sharpen Your Pencil

---
+++ Chapter-10/SharpenYourPencil/SharpenYourPencil/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+
+namespace SharpenYourPencil;
+
+internal class Program
+{
+	static void Main(string[] args)
+	{
+		Console.WriteLine(JsonSerializer.Serialize(3));
+		Console.WriteLine(JsonSerializer.Serialize((long)-3));
+		Console.WriteLine(JsonSerializer.Serialize((byte)0));
+		Console.WriteLine(JsonSerializer.Serialize(float.MaxValue));
+		Console.WriteLine(JsonSerializer.Serialize(float.MinValue));
+		Console.WriteLine(JsonSerializer.Serialize(true));
+		Console.WriteLine(JsonSerializer.Serialize("Elephant"));
+		Console.WriteLine(JsonSerializer.Serialize("Elephant".ToCharArray()));
+		Console.WriteLine(JsonSerializer.Serialize("🐘"));
+	}
+}
