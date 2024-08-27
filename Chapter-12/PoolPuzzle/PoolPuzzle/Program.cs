f55c7654f1e1a653229e1d6b53efe5141ee306dc
patzgn <patrycja.zgierun@gmail.com>
2024-08-27 18:36:48 +0200

    Pool puzzle

---
+++ Chapter-12/PoolPuzzle/PoolPuzzle/Program.cs
@@ -0,0 +1,14 @@
+﻿using PoolPuzzle;
+
+Kangaroo joey = new Kangaroo();
+int koala = joey.Wombat(
+    joey.Wombat(joey.Wombat(1)));
+try
+{
+    Console.WriteLine((15 / koala)
+        + " ggs per pound");
+}
+catch (DivideByZeroException)
+{
+    Console.WriteLine("G'Day Mate!");
+}
